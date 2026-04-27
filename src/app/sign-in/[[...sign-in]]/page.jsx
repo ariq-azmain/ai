ffc1f3b38cdb13d747f72e4c2e11118c57d8b236
ariq-azmain <ariq-azmain123@gmail.com>
2026-04-27 09:58:58 +0600

    fix clark ui

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,9 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />
+    return (
+        <div className="h-screen w-screen flex justify-centeri items-center bg-black">
+            <SignIn />
+        </div>
+    );
 }
